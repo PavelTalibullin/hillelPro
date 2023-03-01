44872b9662e0870ab826488d78ca9ae075d74787
PavelTalibullin <pavlotalibullin@gmail.com>
2023-03-01 15:03:38 +0300

    did homework react

--- src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-const Button = (props) => {
-  console.log(props.children);
-  const { onClick, children = "Default" } = props;
-
-  return <button onClick={onClick}>{children}</button>;
-};
-
-export default Button;
