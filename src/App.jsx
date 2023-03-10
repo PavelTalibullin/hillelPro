1d83985a7ced93bc570f065ac93119e698921f4b
PavelTalibullin <pavlotalibullin@gmail.com>
2023-03-10 18:41:30 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import { Routes, Route, NavLink } from "react-router-dom";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
-import Post from "./pages/Post";
-import NotFound from "./pages/NotFound";
+import { useState, createContext} from "react";
+import Widget from "./components/Widget";
 import "./App.css";
 
+//   1 Create Context
+export const Context = createContext()
+
 const App = () => {
+  const [user, setUser] = useState({ name: "Username" });
+
   return (
     <div className="App">
-      <nav>
-        <NavLink to="/">Home </NavLink>
-        <NavLink to="/about">About </NavLink>
-        <NavLink to="/posts">Posts </NavLink>
-      </nav>
+		<Context.Provider value={user}>
 
-      <h1>App component</h1>
+		<Widget/>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-
-        <Route path="/posts/:id" element={<Post />} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+		</Context.Provider>
     </div>
   );
 };
